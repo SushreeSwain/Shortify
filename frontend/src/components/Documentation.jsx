b04e405e1ca54845acca0d3ce0cef42291e22f96
Sushree S Swain <sushreesswain03@gmail.com>
2025-07-25 18:29:30 +0530

    Edit docs again

--- frontend/src/components/Documentation.jsx
+++ frontend/src/components/Documentation.jsx
@@ -11,16 +11,16 @@ const Documentation = () => {
       <h1 className="text-4xl font-bold mb-4 leading-12">Documentation</h1>
 
       <p className=" text-xl mb-4 leading-10">
-        This is a beginner-friendly URL shortener project built using Node.js, Express.js, MongoDB for the backend, and Vite + React with Tailwind CSS for the frontend.
+        Shortify is a beginner-friendly URL shortener project built using Node.js, Express.js, MongoDB for the backend, and Vite + React with Tailwind CSS for the frontend. This is the official documentation page for Shortify. Students and new developers are free to reference this project for understanding how it works behind the scenes.
       </p>
 
       <h2 className="text-3xl font-semibold mt-6 mb-2 leading-12">Features</h2>
       <ul className=" text-xl list-disc list-inside mb-4 leading-10">
-        <li>Shorten URLs with random or custom aliases</li>
-        <li>Generate QR codes for shortened URLs</li>
-        <li>Track number of visits and URLs created</li>
-        <li>Expiry support for temporary short links</li>
-        <li>404 and Expired page redirection</li>
+        <li>Users can shorten URLs using any custom name, if not then we can generate a random short URL.</li>
+        <li>After creating a short URL, users can click on the "Generate QR Code" if they want to. The QR Code can be downloaded as an png image file.</li>
+        <li>We track the total number of URLs generated as well as how many short URLs have been visited.</li>
+        <li>In case users want the short link to be temporarily available we do have a feature that lets users choose any given date in the future, if not the generated URL will not have any expiration date.  </li>
+        <li>In case users type in any wrong URL, they will be automatically redirected to a 404 error page. If any URL is expired, users will be redirected to a 410 error page. They can return to the homepage in both scenarios and generate another valid short URL.</li>
       </ul>
 
       <h2 className="text-3xl font-semibold mt-6 mb-2 leading-12">Tech Stack</h2>
