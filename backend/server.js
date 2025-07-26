3e7d5b9f16dae799395ff71f7bfc9fa4f16ea6f8
Sushree S Swain <sushreesswain03@gmail.com>
2025-07-26 12:09:41 +0530

    add new url

--- backend/server.js
+++ backend/server.js
@@ -172,7 +172,7 @@ app.get('/:shortId', async (req, res) => {
 
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`✅ Server is running on port ${PORT}`);
 });
